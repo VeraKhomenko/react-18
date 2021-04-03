238614cb6a3972bb0fce6fabc8d750ac6e1fb87f
VeraKhomenko <pochtaVKh@gmail.com>
2021-04-03 20:49:11 +0300

    конспект

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -6,7 +6,8 @@ const TodoList = ({ todos, onDeleteTodo }) => (
     {todos.map(({ id, text }) => (
       <li key={id} className="TodoList__item">
         <p className="TodoList__text">{text}</p>
-        <button onClick={() => onDeleteTodo(id)}>Удалить</button>
+        <button onClick={() =>
+          onDeleteTodo(id)}>Удалить</button>
       </li>
     ))}
   </ul>
